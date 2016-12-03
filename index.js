1491c0bd5696395e39a923c701a8270fc1aa79d0
Shailendra Chauhan <pro.shailendra@gmail.com>
2016-12-03 10:37:50 +0530

    update code

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = {
     NumbertoLocale: function(number, locale) {
         return number.toLocaleString(locale);
+    },
+    Add: function(x, y) {
+        return x + y;
     }
 };
